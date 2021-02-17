4bc2e9555a4460ee59d99a20bfc73103df0bbcf2
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-17 17:03:38 -0500

    Constructed express server

--- server.js
+++ server.js
@@ -2,8 +2,20 @@
 const express = require("express");
 const app = express();
 
+// Declarations
+const routes = require("./routes/routes");
+const bodyParser = require("body-parser");
+
 // Variables
+const PORT = 4000 || process.argv.PORT;
 
 // Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use("/", routes);
 
 // Listening method
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
