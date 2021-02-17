dabf38f18bd29a61c9afc702b069f57fadb15829
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-16 19:23:22 -0500

    Added browser router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home/Home'
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/Home/Home";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <section className='home-container'>
+      <section className="home-container">
         <Home></Home>
       </section>
-      <section className='home-container2'>
+      <section className="home-container2">
         <h1>Section Two</h1>
       </section>
-      <section className='home-container3'>
+      <section className="home-container3">
         <h1>Section Three</h1>
       </section>
     </div>
