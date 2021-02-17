3642a244fccf535702faa237f55ac2d65e2ec36f
cfor0 <cfor0@outlook.com>
2021-02-17 17:30:50 -0500

    added animated arrow with new components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import Navigation from "../../components/Navigation/Navigation.js";
 import Title from "../../components/Title/Title";
+import ScrollArrow from "../../components/ScrollArrow/ScrollArrow";
 
 function Home(props) {
   return (
@@ -13,15 +14,18 @@ function Home(props) {
               title = 'Model S'
               caption = 'Order Online for Touchless Delivery'            
             />
+            <ScrollArrow />
         </section>
         <section className="home-container2">
         <Title
-              title = 'Model Y'            
+              title = 'Model Y'
+              caption = 'Order Online for Touchless Delivery'              
             />
         </section>
         <section className="home-container3">
         <Title
-              title = 'Model 3'            
+              title = 'Model 3' 
+              caption = 'Order Online for Touchless Delivery'             
             />
         </section>
       </div>
