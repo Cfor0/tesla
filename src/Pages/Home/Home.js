aa8e5b5e32182d13a38b2709447e45b31e7f5bb3
cfor0 <cfor0@outlook.com>
2021-02-17 20:35:01 -0500

    added footer and css

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,31 +3,36 @@ import "./Home.css";
 import Navigation from "../../components/Navigation/Navigation.js";
 import Title from "../../components/Title/Title";
 import ScrollArrow from "../../components/ScrollArrow/ScrollArrow";
+import Footer from '../../components/Footer/Footer';
 
-function Home(props) {
+function Home() {
   return (
     <>
       {/* <Navigation /> */}
       <div className="App">
-        <section className="home-container">
+        <section className="home-container home-image">
+          <Title
+            title='Model S'
+            caption='Order Online for Touchless Delivery'
+          />
+          <ScrollArrow />
+        </section>
+        <section className="home-container2 home-image">
+          <Title
+            title='Model Y'
+            caption='Order Online for Touchless Delivery'
+          />
+        </section>
+        <section className="home-container3 home-image">
+          <div className='last-container'>
             <Title
-              title = 'Model S'
-              caption = 'Order Online for Touchless Delivery'            
+              title='Model 3'
+              caption='Order Online for Touchless Delivery'
             />
-            <ScrollArrow />
+          </div>
+          <Footer />
         </section>
-        <section className="home-container2">
-        <Title
-              title = 'Model Y'
-              caption = 'Order Online for Touchless Delivery'              
-            />
-        </section>
-        <section className="home-container3">
-        <Title
-              title = 'Model 3' 
-              caption = 'Order Online for Touchless Delivery'             
-            />
-        </section>
+
       </div>
     </>
   );
