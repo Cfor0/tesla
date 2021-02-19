4f173519c9fc4cc4c772eb8a10ff8f33b79cab84
Jaquelinlara <lara.jackie09@gmail.com>
2021-02-19 13:37:58 -0500

    Create account page

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/:car" component={CarModel} />
       <Route exact path="/login" component={Registration} />
+      <Route exact path="/:models" component={CarModel} />
       <Route exact path="/store" component={Store} />
     </Switch>
   );
