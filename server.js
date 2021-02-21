1bb8ec47662de5622939825e7135285306299dc5
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-21 15:46:29 -0500

    Added db connection structure

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 // Express declaration
 const express = require("express");
-const app = express();
 
 // Declarations
 const routes = require("./routes/routes");
-const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Variables
 const PORT = 4000 || process.argv.PORT;
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // Routes
 app.use("/", routes);
 
 // Listening method
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+    console.log(`Server listening on http://localhost:${PORT}`);
 });
