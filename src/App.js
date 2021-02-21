6d51c7400b5739ee856ec0c1bdd24cca76ccded7
jorge <jorgealejandrovaca@gmail.com>
2021-02-21 10:08:32 -0500

    contact.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Pages/Home/Home";
 import CarModel from "./Pages/CarModel/CarModel";
 import Registration from "./Pages/Registration/Registration";
 import Store from "./Pages/Store/Store";
+import Contact from "./Pages/Contact/Contact";
 import { Route, Switch } from "react-router-dom";
 
 function App() {
