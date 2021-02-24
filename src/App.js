6b89afef5a1e9215000ff307660676177556b4eb
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-24 16:41:31 -0500

    Added loggedin component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Home from "./Pages/Home/Home";
 import CarModel from "./Pages/CarModel/CarModel";
 import Registration from "./Pages/Registration/Registration";
 import Calculator from "./Pages/Calculator/Calculator";
-import Contact from "./Pages/Contact/Contact"
+import Contact from "./Pages/Contact/Contact";
+import LoggedIn from "./Pages/LoggedIn/LoggedIn";
 
 import { Route, Switch } from "react-router-dom";
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Registration} />
+            <Route exact path="/logged-in" component={LoggedIn} />
             <Route exact path="/models" component={CarModel} />
             <Route exact path="/calculator" component={Calculator} />
             <Route exact path="/contact" component={Contact} />
