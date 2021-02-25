467422599b1e4e294aaea667c10489de414e47c1
Natalie Rodriguez <72086492+NatalieRodriguez535@users.noreply.github.com>
2021-02-25 17:16:21 -0500

    Logged in

--- src/Pages/LoggedIn/LoggedIn.js
+++ src/Pages/LoggedIn/LoggedIn.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./LoggedIn.css";
 
+function clickMe() {
+    alert("You clicked me!");
+}
+
 const LoggedIn = () => {
     return (
-        <div id="logged-in__main-container">
-            <h1>Logged In</h1>
+        <div className="main-container">
+            <div className="mc-login">
+                <div className="logged-in__second-container">
+                    <h1 className="logged-in__h1">You Just Landed!</h1>
+
+                    <button className="button-one" onClick={clickMe}>
+                        Sign Off
+                    </button>
+                    <button className="button-two" onClick={clickMe}>
+                        Take A Look
+                    </button>
+                </div>
+                <div className="loggedin-img"></div>
+            </div>
         </div>
     );
 };
